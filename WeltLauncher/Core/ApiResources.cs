7355363c9d383c043288502c874222dad76642a7
conji <dubstepandponies95@gmail.com>
2016-06-05 14:33:06 -0700

    updated api resources

--- WeltLauncher/Core/ApiResources.cs
+++ WeltLauncher/Core/ApiResources.cs
@@ -6,21 +6,27 @@ namespace WeltLauncher.Core
     public class ApiResources
     {
         private const string API_V1 = "api/v1/";
-        private const string CHANGELOG = API_V1 + "changelog/";
-        private const string AUTH_GET = API_V1 + "auth/get/";
-        private const string AUTH_LOG = API_V1 + "auth/login/";
-        private const string AUTH_REG = API_V1 + "auth/register/";
+        public const string CHANGELOG = API_V1 + "changelog/";
+        public const string AUTH_GET = API_V1 + "auth/get/";
+        public const string AUTH_LOG = API_V1 + "auth/login/";
+        public const string AUTH_REG = API_V1 + "auth/register/";
+        public const string RESX_OBJ = API_V1 + "resx/";
+        public const string RESX_VER = API_V1 + "resx/version/";
+        public const string RESX_LAUNCHER_VER = API_V1 + "/resx/launcherversion/";
+        public const string RESX_DL = API_V1 + "resx/download/";
+        public const string RESX_LAUNCHER = API_V1 + "resx/launcher/";
 
         public const string TEST_URL = "http://localhost:1823/";
-        public const string TEST_CHANGELOG_URL = TEST_URL + CHANGELOG;
-        public const string TEST_USER_GET_URL = TEST_URL + AUTH_GET;
-        public const string TEST_USER_REG_URL = TEST_URL + AUTH_REG;
-        public const string TEST_USER_LOG_URL = TEST_URL + AUTH_LOG;
 
         public const string RELEASE_URL = "http://weltsite.azurewebsites.net/";
-        public const string RELEASE_CHANGELOG_URL = RELEASE_URL + CHANGELOG;
-        public const string RELEASE_USER_GET_URL = RELEASE_URL + AUTH_GET;
-        public const string RELEASE_USER_LOG_URL = RELEASE_URL + AUTH_LOG;
-        public const string RELEASE_USER_REG_URL = RELEASE_URL + AUTH_REG;
+
+        public static string GetUrl(string endpoint)
+        {
+#if DEBUG
+            return TEST_URL + endpoint;
+#else
+            return RELEASE_URL + endpoint;
+#endif
+        }
     }
 }
