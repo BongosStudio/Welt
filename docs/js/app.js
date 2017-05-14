693a90150b5e84123050f2826304c96ff94ce6e9
Justin Cox <dubstepandponies95@gmail.com>
2017-05-13 21:49:29 -0600

    Another change cause fuck it

--- docs/js/app.js
+++ docs/js/app.js
@@ -25,6 +25,23 @@ $('.main').onepage_scroll({
     setTimeout(function() {
       $('section.active').toggleClass('inview', true);
     }, 250);
+    var color = $('body').css('background-color');
+    switch(index) {
+      case 0:
+        color = 'rgb(35,0,50);'
+        break;
+      case 1:
+        color = 'rgb(40,0,55);'
+        break;
+      case 2:
+        color = 'rgb(45,0,60);'
+        break;
+      case 3:
+        color = 'rgb(50,0,65);'
+        break;
+    }
+    $('body').css('transition', 'background-color ease 1s;');
+    $('body').css('background-color', color);
   },
   loop: false,
   keyboard: true,
